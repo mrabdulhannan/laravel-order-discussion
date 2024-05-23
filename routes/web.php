ddd3b071c6f5d3808cba5926ed253886901a3ea1
abdulhannandanish <mrahdanish@gmail.com>
2024-05-23 17:14:43 +0500

    added missing routes

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,16 @@ Route::get('/', function () {
 });
 
 
+// Route::get('/chats/{order_id}', [ChatController::class, 'getMessages']);
+Route::post('/createchats/{order_id}', [ChatController::class, 'sendMessage']);
+Route::post('/createchats/{order_id}', [ChatController::class, 'sendMessage']);
+
+// Route::get('/messenger/{order_id}', function () {
+//     return view('welcome');
+// });
+
+Route::get('/messenger', [ChatController::class, 'openMessenger']);
+
+//CRUD Operations on Messages
+Route::get('/messages/create', [ChatController::class, 'create'])->name('messages.create');
+Route::post('/messages', [ChatController::class, 'store'])->name('messages.store');
